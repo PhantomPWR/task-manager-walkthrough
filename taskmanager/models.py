6c08ff679ef73c1306f10fa5f148bf4daf5b0f0c
Jean de Villiers <jeandevilliers@me.com>
2022-12-30 17:05:46 +0000

    Finish database & schema

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,17 +2,18 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for Category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    category_name = db.Column(db.string(25, unique=True, nullable=False))
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
-    def __repr(self):
+    def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.category_name
 
 
 class Task(db.Model):
-    # schema for Task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
     task_description = db.Column(db.Text, nullable=False)
@@ -20,6 +21,8 @@ class Task(db.Model):
     due_date = db.Column(db.Date, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
-    def __repr(self):
+    def __repr__(self):
         # __repr__ to represent itself in the form of a string
-        return self
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
