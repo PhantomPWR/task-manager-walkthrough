5dd04daaae4e38b4e5642557dcf25915ef0909d6
Jean de Villiers <jeandevilliers@me.com>
2022-12-31 17:01:23 +0000

    Add functionality to delete records - with modal confirmation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // modal initialization
+    let modals = document.querySelectorAll('.modal');
+    M.Modal.init(modals);
 });
 
   // Or with jQuery
