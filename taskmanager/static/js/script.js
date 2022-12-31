1dec7f2961d32faf5287f6f6e1f659cf05b5e017
Jean de Villiers <jeandevilliers@me.com>
2022-12-31 17:57:34 +0000

    Add functionality for adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -6,6 +6,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // modal initialization
     let modals = document.querySelectorAll('.modal');
     M.Modal.init(modals);
+
+    // date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select dropdown initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
 
   // Or with jQuery
