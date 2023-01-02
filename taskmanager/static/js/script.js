c48c87facda5e86e82a91627f075cf35396a807e
Jean de Villiers <jeandevilliers@me.com>
2023-01-02 12:45:06 +0000

    Add functionality for editing tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
     // date picker
     let datepicker = document.querySelectorAll('.datepicker');
     M.Datepicker.init(datepicker, {
-      format: "dd mmmm, yyyy",
+      format: "dd mmmm yyyy",
       i18n: {done: "Select"}
     });
 
